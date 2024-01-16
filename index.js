33fc5cef02c1f85b2bd5d3324c8a9c4772f107be
Jessica Lee <72154043+jessicalee2005@users.noreply.github.com>
2024-01-15 22:08:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function myName() {
-    document.getElementById("myName").innerHTML = "In my free time I like visiting new places and eating new food! My hometown is Cerritos, located in Southern California"
+    document.getElementById("myName").innerHTML = "In my free time I like visiting new places and eating new food! My hometown is Cerritos, located in Southern California."
 }
 
